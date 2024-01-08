f968240ab1d99f270ebf2d1225eb53da28829c18
KarthikeyanKandan <akashkarthiofficial@gmail.com>
2024-01-08 17:31:13 -0400

    added scroll to compare section

--- script.js
+++ script.js
@@ -82,3 +82,23 @@ function render() {
   // Draw the second set of images, potentially adjusting the position based on your desired overlap
   context.drawImage(images2[airpods2.frame], x2, y2, imgWidth2, imgHeight2);
 }
+
+
+gsap.utils.toArray(".comparisonSection").forEach(section => {
+	let tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: section,
+				start: "center center",
+        // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
+				end: () => "+=" + section.offsetWidth, 
+				scrub: true,
+				pin: true,
+        anticipatePin: 1
+			},
+			defaults: {ease: "none"}
+		});
+	// animate the container one way...
+	tl.fromTo(section.querySelector(".afterImage"), { xPercent: 100, x: 0}, {xPercent: 0})
+	  // ...and the image the opposite way (at the same time)
+	  .fromTo(section.querySelector(".afterImage img"), {xPercent: -100, x: 0}, {xPercent: 0}, 0);
+});
